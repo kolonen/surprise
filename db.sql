e96b21305768dd1b56dcb6c7f324829fcd7bd757
= <timo.kolonen@gmail.com>
2015-08-06 21:42:11 +0300

    add keys to db

--- db.sql
+++ db.sql
@@ -1,14 +1,18 @@
+DROP TABLE IF EXISTS event;
+DROP TABLE IF EXISTS wager;
+
 CREATE TABLE wager(
-  wager_id INT,
+  wager_id INT NOT NULL AUTO_INCREMENT,
   wager_date DATETIME,
   manager VARCHAR(32),
   system_size INT,
-  bet INT         
+  bet INT,
+  PRIMARY KEY (wager_id)         
 );
 
 CREATE TABLE event(
-  event_id INT,
-  wager_id INT,
+  event_id INT NOT NULL AUTO_INCREMENT,
+  wager_id INT NOT NULL,
   insert_date DATETIME,
   author VARCHAR(32),
   choose_home BOOLEAN DEFAULT false,
@@ -17,5 +21,7 @@ CREATE TABLE event(
   home_team VARCHAR(32),
   away_team VARCHAR(32),
   home_score INT,
-  away_score INT
+  away_score INT,
+  PRIMARY KEY (event_id),
+  FOREIGN KEY (wager_id) REFERENCES wager(wager_id)
 );
