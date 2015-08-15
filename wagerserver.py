3e1a35d7a442ec9d707af43efb63443c685b2227
storniai <sami.torniainen@wippies.fi>
2015-08-16 02:29:35 +0300

    rest wagers to json
    wagers with events can be fetched via rest API using the following url:
    http://localhost:5000/wagers

--- wagerserver.py
+++ wagerserver.py
@@ -2,26 +2,45 @@ from flask import Flask
 from flask import Response
 from flask import request
 from database import database
+from surprise import wager
+from surprise import event
 import json
+import simplejson
 
 app = Flask(__name__)
-
-@app.before_request
-def db_connect():
-    db = database()
+db = database()
 
 @app.after_request
 def db_disconnect(response):
-  return response
+    return response
 
 @app.route("/")
 def hello():
-  return "Hello World!"
+    return "Hello World!"
 
 @app.route("/wagers", methods=['GET'])
 def names():
-    # todo: add database query here
-  return "Hello World, here come wagers!"
+    def dict_from_wager(w):
+        d = {"wager_id" : w.wager_id,
+        "ext_id" : w.ext_id,
+        "wager_date" : str( w.wager_date ),
+        "manager" : w.manager,
+        "system_size" : w.system_size,
+        "stake" : w.stake,
+        "win_amount" : w.win_amount,
+        "events" : []}
 
+        for e in w.events:
+            d['events'].append(e.__dict__)
+        return d
+    #todo: get manager name from request
+    wagers = db.get_wagers("Mixu Paatelainen")
+    d = []
+    for wager in wagers:
+        d.append( dict_from_wager(wager) )
+    data = json.dumps(d, indent=4)
+    resp = Response(data, status=200, mimetype='application/json')
+    return resp
+        
 if __name__ == "__main__":
-  app.run()
+    app.run()
