1a8ed71f38c40d576f7a026de5fbaed991ee032f
storniai <sami.torniainen@wippies.fi>
2015-08-17 00:05:57 +0300

    rest get wagers
    manager name can be given in url. For example:
    http://localhost:5000/wagers?manager=Mixu+Paatelainen

--- wagerserver.py
+++ wagerserver.py
@@ -11,10 +11,13 @@ def hello():
 
 @app.route("/wagers", methods=['GET'])
 def wagers():
-    #todo: get manager name from request
-    wagers = db.get_wagers("Mixu Paatelainen")
-    wagers_json = json.dumps(map(lambda x: x.to_dict(   ), wagers), indent=4)
-    return flask.Response(wagers_json, status=200, mimetype='application/json')
+    if 'manager' in flask.request.args:        
+        wagers = db.get_wagers(flask.request.args['manager'])
+        wagers_json = json.dumps(map(lambda x: x.to_dict(   ), wagers), indent=4)
+        return flask.Response(wagers_json, status=200, mimetype='application/json')
+    else:
+        message = {'Not Found: ' + flask.request.url}   
+        return flask.Response(message, status=404, mimetype='application/json')
      
 if __name__ == "__main__":
     app.run(debug=True)
