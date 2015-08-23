5a33a52fc3fd2b9968c84989635cfb729a5c519f
= <timo.kolonen@gmail.com>
2015-08-23 21:51:58 +0300

    archive this

--- client/src/Surprise.js
+++ client/src/Surprise.js
@@ -10,12 +10,25 @@ var Surprise = React.createClass ({
             this.setState({
 		wagers: r,
             })
-	    console.log(r)
 	}.bind(this));
     },
     render: function() {
-        return (<div>Surprise Surprise {this.state.wagers}</div>)
+	//var w = this.state.wagers[0]
+	var w = {wager_date: "a", ext_id: "b", win_amount: "c"}
+        console.log(_.map(this.state.wagers, function(s) {return s.ext_id}))
+	return (
+		
+		<div><Wager wager_date={w.wager_date} ext_id={w.ext_id} win_amount ={w.win_amount}/></div>
+	) 
     }
 })
+var Wager = React.createClass ({
+    
+    render: function() {
+	return (<div>{this.props.wager_date}, {this.props.ext_id}, {this.props.win_amount}</div>)
+    }
+})
+
+
 
 React.render(<Surprise/>, document.getElementById("surprise"))  
