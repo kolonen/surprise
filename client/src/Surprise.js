0619de365dd9c7979afa0a68a6d665b001c56ee0
= <timo.kolonen@gmail.com>
2015-08-24 21:41:23 +0300

    some wagers on the page

--- client/src/Surprise.js
+++ client/src/Surprise.js
@@ -13,13 +13,15 @@ var Surprise = React.createClass ({
 	}.bind(this));
     },
     render: function() {
-	//var w = this.state.wagers[0]
-	var w = {wager_date: "a", ext_id: "b", win_amount: "c"}
-        console.log(_.map(this.state.wagers, function(s) {return s.ext_id}))
 	return (
-		
-		<div><Wager wager_date={w.wager_date} ext_id={w.ext_id} win_amount ={w.win_amount}/></div>
-	) 
+		<div>	
+		{
+		    _.map(this.state.wagers, 
+		       function(w) {return <Wager wager_date={w.wager_date} ext_id={w.ext_id} win_amount ={w.win_amount}/>}
+		      )
+		}
+		</div>
+	)	
     }
 })
 var Wager = React.createClass ({
@@ -28,7 +30,6 @@ var Wager = React.createClass ({
 	return (<div>{this.props.wager_date}, {this.props.ext_id}, {this.props.win_amount}</div>)
     }
 })
-
 
 
 React.render(<Surprise/>, document.getElementById("surprise"))  
