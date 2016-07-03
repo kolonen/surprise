804209e42dd2ed4571aef812a6bf615144f500fa
Sami Torniainen <sami.2.torniainen@gmail.com>
2016-07-03 16:48:34 +0300

    ingone key score if not exists in json

--- server/wager_client.py
+++ server/wager_client.py
@@ -20,7 +20,7 @@ def login (username, password):
     if r.status_code == 200:
         return s
     else:
-        raise Exception("Authentication failed", r.status_code)    
+        raise Exception("Authentication failed", r.status_code)
 
 def get_wagers(session, count=10):
     tx_ids = get_tx_ids(session, count)
@@ -30,7 +30,7 @@ def get_tx_ids(session, count):
     r = session.get('https://www.veikkaus.fi/api/v1/players/self/account/transactions?size='+str(count)+'&sort-by=RESULT_DATE', headers=headers)
     txs = r.json()['transactions']
     return map(lambda x: x['externalId'], txs)
-    
+
 def get_wagers_by_txs(session, txs):
     wagers = map(lambda x: get_wager(session,x), txs)
     wagers = filter(lambda x: 'draw' in x and x['draw']['gameName'] == 'SPORT', wagers)
@@ -40,52 +40,52 @@ def get_wagers_by_txs(session, txs):
 def get_wager(session, tx_id):
         r = session.get('https://www.veikkaus.fi/api/v1/sport-games/wagers/'+tx_id, headers=headers)
         return json.loads(r.text)
-        
+
 def parse_wager(w):
-    
+
     def parse_selection(s):
         return {
-            'home': (True if 'home' in s else False), 
+            'home': (True if 'home' in s else False),
             'tie': (True if 'tie' in s else False),
-            'away': (True if 'away' in s else False) 
+            'away': (True if 'away' in s else False)
         }
     def parse_datetime(t):
         return time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(t/1000))
-    
+
     #parse selections
     rawSelections = w['wager']['selections'][0]['outcomes']
-    selections = enumerate(map(lambda s: parse_selection(s), rawSelections)) 
-    selections = map(lambda (i, s): (i,s), selections) 
-    
-    #make dictionary with id as a key for easy addressing 
-    #(even though the array is probably in order anyway) 
+    selections = enumerate(map(lambda s: parse_selection(s), rawSelections))
+    selections = map(lambda (i, s): (i,s), selections)
+
+    #make dictionary with id as a key for easy addressing
+    #(even though the array is probably in order anyway)
     rows = dict(map(lambda r: (int(r['id']), r), w['draw']['rows']))
-    
-    events = map(lambda (i,s): 
-                 surprise.event(
+
+    def getScore(d,k):
+        if 'score' in d:return d['result']['score'][k]
+
+    events = map(lambda (i,s):
+                surprise.event(
                      row_id=i,
                      choose_home=s['home'],
                      choose_tie=s['tie'],
                      choose_away=s['away'],
                      home_team=rows[i]['outcome']['home']['name'],
                      away_team=rows[i]['outcome']['away']['name'],
-                     home_score=rows[i]['result']['score']['home'],
-                     away_score=rows[i]['result']['score']['away']
+                     home_score=getScore(rows[i],'home'),
+                     away_score=getScore(rows[i],'away')
                  ), selections)
-    
+
     #parse wager high level data, create wager
     wager_date = parse_datetime(w['wager']['transactionTime'])
     external_id = w['wager']['id']
     stake = w['wager']['stake']
     system_size = w['wager']['systemLevel']
     win_amount = w['winning']['amount']
-        
-    return surprise.wager(wager_date=wager_date, 
+
+    return surprise.wager(wager_date=wager_date,
                           ext_id=external_id,
-                          stake=stake, 
+                          stake=stake,
                           system_size=system_size,
                           win_amount = win_amount,
                           events=events)
-    
-
-    
