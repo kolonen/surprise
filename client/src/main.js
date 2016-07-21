d7010004e63198e1d9c81ba9d4b53086f77454df
= <timo.kolonen@gmail.com>
2016-07-21 23:04:28 +0300

    react dep was needed

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,5 @@
 const Surprise = require('./Surprise.js')
 const ReactDom = require('react-dom');
+const React = require('react')
 
 ReactDom.render(<Surprise/>, document.getElementById("surprise"))
